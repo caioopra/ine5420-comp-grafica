71cf9ee0fa273023777bbab63ab952f5730997e2
Pedro <pedro.nackm@gmail.com>
2023-09-28 21:05:57 -0300

    arrumando liang_barsky de novo

--- GraphicalSystem2D/src/utils/clipping/LiangBarsky.py
+++ GraphicalSystem2D/src/utils/clipping/LiangBarsky.py
@@ -17,7 +17,7 @@ def liang_barsky(line: Line, window_max: Point = Point(1, 1), window_min: Point 
     inside = False
 
     for i in range(4):
-        if p[i] == 0 and q[i] <= 0:
+        if p[i] == 0 and q[i] < 0:
             return None
     
     positivos = []
@@ -25,17 +25,23 @@ def liang_barsky(line: Line, window_max: Point = Point(1, 1), window_min: Point 
     for i in range(4):
         if p[i] > 0:
             positivos.append(i)
-        else:
+        elif p[i] < 0:
             negativos.append(i)
 
     # interseccao de fora pra dentro
     r1 = q[negativos[0]] / p[negativos[0]]
-    r2 = q[negativos[1]] / p[negativos[1]]
+    if len(negativos) > 1:
+        r2 = q[negativos[1]] / p[negativos[1]]
+    else:
+        r2 = 0
     zeta_1 = max(0, r1, r2) # se der 0, interseccao nao existe e valores sao rejeitados
 
     # interseccao de dentro pra fora
     r3 = q[positivos[0]] / p[positivos[0]]
-    r4 = q[positivos[1]] / p[positivos[1]]
+    if len(positivos) > 1:
+        r4 = q[positivos[1]] / p[positivos[1]]
+    else:
+        r4 = 1
     zeta_2 = min(1, r3, r4) # se der 1, interseccao nao existe e valores sao rejeitados
 
     # se zeta 1 > zeta 2, a linha ta completamente fora
@@ -52,4 +58,5 @@ def liang_barsky(line: Line, window_max: Point = Point(1, 1), window_min: Point 
     point_1 = Point(new_x1, new_y1)
     point_2 = Point(new_x2, new_y2)
     print(point_1, point_2)
-    return Line(point_1, point_2)
+    line.setNormalCoordinates(point1, point2) 
+    return line
