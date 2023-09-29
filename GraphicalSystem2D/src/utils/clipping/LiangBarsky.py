cf54f1c46d2e93391d3b4a400ea5e6ba8be65fa5
Pedro <pedro.nackm@gmail.com>
2023-09-28 23:35:46 -0300

    LIANG FINALMENTE FUNCIONANDO

--- GraphicalSystem2D/src/utils/clipping/LiangBarsky.py
+++ GraphicalSystem2D/src/utils/clipping/LiangBarsky.py
@@ -14,39 +14,18 @@ def liang_barsky(line: Line, window_max: Point = Point(1, 1), window_min: Point 
     p = [- delta_x, delta_x, - delta_y, delta_y]
     q = [point1.getNormalX() - xw_min, xw_max - point1.getNormalX(), point1.getNormalY() - yw_min, yw_max - point1.getNormalY()]
 
-    inside = False
-
     for i in range(4):
         if p[i] == 0 and q[i] < 0:
             return None
-    
-    positivos = []
-    negativos = []
-    for i in range(4):
-        if p[i] > 0:
-            positivos.append(i)
-        elif p[i] < 0:
-            negativos.append(i)
 
-    # interseccao de fora pra dentro
-    r1 = q[negativos[0]] / p[negativos[0]]
-    if len(negativos) > 1:
-        r2 = q[negativos[1]] / p[negativos[1]]
-    else:
-        r2 = 0
-    zeta_1 = max(0, r1, r2) # se der 0, interseccao nao existe e valores sao rejeitados
-
-    # interseccao de dentro pra fora
-    r3 = q[positivos[0]] / p[positivos[0]]
-    if len(positivos) > 1:
-        r4 = q[positivos[1]] / p[positivos[1]]
-    else:
-        r4 = 1
-    zeta_2 = min(1, r3, r4) # se der 1, interseccao nao existe e valores sao rejeitados
+    pq = list(zip(p, q))
+    negativos = [(q / p) for (p, q) in pq if p < 0]
+    zeta_1 = max(0, max(negativos, default=0))
+    positivos = [(q / p) for (p, q) in pq if p > 0]
+    zeta_2 = min(1, min(positivos, default=1))
 
     # se zeta 1 > zeta 2, a linha ta completamente fora
     if zeta_1 > zeta_2:
-        print("completamente fora")
         return None
 
     new_x1 = point1.getNormalX() + zeta_1 * delta_x
@@ -55,8 +34,6 @@ def liang_barsky(line: Line, window_max: Point = Point(1, 1), window_min: Point 
     new_x2 = point1.getNormalX() + zeta_2 * delta_x
     new_y2 = point1.getNormalY() + zeta_2 * delta_y
 
-    point_1 = Point(new_x1, new_y1)
-    point_2 = Point(new_x2, new_y2)
-    print(point_1, point_2)
-    line.setNormalCoordinates(point1, point2) 
+    point1.setNormalCoordinates(new_x1, new_y1)
+    point2.setNormalCoordinates(new_x2, new_y2)
     return line
